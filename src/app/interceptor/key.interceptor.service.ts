40a0283645d49776176aa7921acc02a0a4eb7039
Ray <ray.hancock.c@gmail.com>
2018-08-07 20:16:56 -0400

    header change

--- src/app/interceptor/key.interceptor.service.ts
+++ src/app/interceptor/key.interceptor.service.ts
@@ -7,17 +7,24 @@ export class KeyInterceptorService implements HttpInterceptor {
 
     yelpKey = "Wa29AFf1zWalIPDVYUkN3eRIvg9ztpotJVoM0MzTJ8jgdNy-5SY00xsn5dTRQsV854NU4o8kd0G0Hhktu63dFtOoMYdQW6eEZzHFxbyuzhJyJ4seUm9hIaMFLW9oW3Yx";
 
-
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        
-        const headers = new HttpHeaders({
-            "Content-Type": "application/json",
-            'Access-Control-Allow-Origin':'https://yelpapi-client.herokuapp.com/',
-            'Authorization': `bearer ${this.yelpKey}`
-        });
 
-        const cloneReq = req.clone({headers});
-        return next.handle(cloneReq);
+        let headers = req.headers
+            .set('Access-Control-Allow-Origin', 'https://yelpapi-client.herokuapp.com/')
+            .set('Authorization', `bearer ${this.yelpKey}`);
+
+        const cloneReq = req.clone({ headers });
+
+        return next.handle(cloneReq)
+        
+        // const headers = new HttpHeaders({
+        //     "Content-Type": "application/json",
+        //     'Access-Control-Allow-Origin':'https://yelpapi-client.herokuapp.com/',
+        //     'Authorization': `bearer ${this.yelpKey}`
+        // });
+
+        // const cloneReq = req.clone({headers});
+        // return next.handle(cloneReq);
         
         // const authHeader = req.clone({ 
         //     headers: req.headers.set('Authorization', `bearer ${this.yelpKey}`),
