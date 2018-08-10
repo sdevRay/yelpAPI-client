3dbbb748335e74eeee213c7e17da15aed58b5f83
Ray <ray.hancock.c@gmail.com>
2018-08-10 14:08:31 -0400

    test: attempt to connect to server.js from heroku

--- middleware/headers.js
+++ middleware/headers.js
@@ -7,6 +7,6 @@ module.exports = (req, res, next) => {
     if (req.method == "OPTIONS") {
         res.status(200).json();
     } else {
-        next()
+        next();
     }
 }
--- server.js
+++ server.js
@@ -4,16 +4,14 @@ const path = require("path");
 const cors = require("cors");
 const app = express();
 
-
-app.use(require("./middleware/headers"));
-app.use(cors());
-
-
 // Serve only the static files from the dist directory
 app.use(express.static(__dirname + "/dist/yelpAPI-client"));
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname + "/dist/yelpAPI-client/index.html"));
 });
+
+app.use(cors());
+app.use(require("./middleware/headers"));
 
 // TEST ENDPOINT
 app.use("/api", function(req, res){
@@ -21,7 +19,7 @@ app.use("/api", function(req, res){
 })
 // Start the app by listening on the default Heroku port
 app.listen(4200 || 5432, function(){
-    console.log("App running.")
+    console.log("App running.");
 });
 
 // app.listen(process.env.PORT || 5432);
