674d971fb9ef06a5dd14a34a4075cc9f507fd24d
sdevRay <37095603+sdevRay@users.noreply.github.com>
2018-08-12 12:54:43 -0400

    rebuilt server.js to have endpoints

--- server.js
+++ server.js
@@ -1,17 +1,63 @@
+// // Initialize express server
+// const express = require("express");
+// const path = require("path");
+// const app = express();
+
+// // Serve only the static files from the dist directory
+// app.use(express.static(__dirname + "/dist/yelpAPI-client"));
+
+// // Send all requests to index.html
+// app.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname + "/dist/yelpAPI-client/index.html"));
+// });
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 5432, function(){
+//     console.log("App running.");
+// });
+
+////////////////////////////
+
+require("dotenv").config() // dotenv provides a way to allow you to create secret keys that your application needs to function and keep them from going public.
+
 // Initialize express server
 const express = require("express");
 const path = require("path");
 const app = express();
 
+const fetch = require("node-fetch");
+// app.use(require("./middleware/headers"));
+
 // Serve only the static files from the dist directory
 app.use(express.static(__dirname + "/dist/yelpAPI-client"));
 
+app.use(function(req, res, next){
+    res.header('access-control-allow-origin', '*');
+    res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
+    res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+})
+
+app.get('/yelp/test', (req, res) => {
+    fetch("https://api.yelp.com/v3/businesses/search?location=anderson,in", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "bearer " + process.env.YELPKEY
+        }
+    })
+        .then(response => { return response.json(); })
+        .then(result => { res.send(result); })
+        .catch(err => console.log('Error meesage', err));
+});
+
+// Send all requests to index.html
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname + "/dist/yelpAPI-client/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5432, function(){
+app.listen(4200, function(){
     console.log("App running.");
 });
 
--- src/app/service/yelp.service.ts
+++ src/app/service/yelp.service.ts
@@ -13,7 +13,7 @@ export class YelpService {
   private _url: string = "/assets/data/businesses.json";
 
   private serverURL = "https://yelpapi-client.herokuapp.com";
-  private localURL = "http://localhost:3000"
+  private localURL = "http://localhost:4200"
 
   constructor(private http: HttpClient) { }
 
@@ -23,7 +23,7 @@ export class YelpService {
     // return this.http.get(`${this.localURL}/yelp/${pricePoint}/${city}/${state}`);
 
     return this.http.get(`${this.localURL}/yelp/test`)
-    // return this.http.get(`${this.serverURL}/api`);
+    // return this.http.get(`${this.serverURL}/yelp/test`);
   }
 }
 
