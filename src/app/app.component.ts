520b9911f00917f7e69202d84dfb64246d9d81ae
Ray <ray.hancock.c@gmail.com>
2018-08-17 01:32:48 -0400

    WIP: adding responsivness to search bar and cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { YelpService } from './service/yelp.service';
 import { Business } from "./model/business.model";
 import { PricePoint } from "./model/pricepoint.model";
